6f4dc1490a981a4848da9ff0ea6feee3634ec671
pierobassa <piero.bassa@nttdata.com>
2022-11-28 21:35:09 +0100

    [new] Serializing & Deserializing Block for BadgerDB key-value DB which only handles bytes.

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,6 +1,10 @@
 package blockchain
 
-
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
 type Block struct {
 	Hash     []byte
@@ -26,8 +30,6 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-
-
 /*
 We need a Genesis block due to the fact that each block references to a previous block.
 */
@@ -35,3 +37,42 @@ func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{}) //Genesis block will have an empty slice of bytes as the previous Hash
 }
 
+/* -------------- SERIALIZATION & DESERIALIZATION -------------- */
+//BadgerDB only accepts bytes as keys and values. We need a way to serialize and deserialize block data being stored
+
+/*
+Serialize() is a method of the Block struct
+
+@returns: slice of bytes representing the serialization of the block
+*/
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+
+	encoder := gob.NewEncoder(&res) //Package gob manages streams of gobs - binary values exchanged between an Encoder (transmitter) and a Decoder (receiver).
+
+	err := encoder.Encode(b)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return res.Bytes() //returning directly res because we encoded res by passing a reference to res when creating the Encoder
+}
+
+/*
+@parameters: slice of bytes which represents a block that has been encoded and needs to be decoded
+@returns pointer to the Block
+*/
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
+}
