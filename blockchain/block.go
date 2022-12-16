5d01a43c0edd58cd3b155e5164ca859d735bb69c
pierobassa <piero.bassa@nttdata.com>
2022-12-16 21:20:38 +0100

    [fix] Fixed Block initialization & added HashTransactions of a block

--- blockchain/block.go
+++ blockchain/block.go
@@ -2,13 +2,14 @@ package blockchain
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/gob"
 	"log"
 )
 
 type Block struct {
 	Hash         []byte
-	Transactions []*Transaction //Array of transactions. A block may contain many transactions
+	Transactions []*Transaction //Array of transactions. A block must contain at least 1 transaction
 	PrevHash     []byte
 	Nonce        int //The nonce is the number that blockchain miners are solving for.
 }
@@ -17,8 +18,8 @@ type Block struct {
 *
 @returns new pointer to a Block
 */
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash, 0} //block is a reference (&) to a block created with it's constructor. For now Hash is an empty bytes array
+func CreateBlock(txs []*Transaction, prevHash []byte) *Block {
+	block := &Block{[]byte{}, txs, prevHash, 0} //block is a reference (&) to a block created with it's constructor.
 
 	//Running the Proof of Work algorithm on the block
 	pow := NewProof(block)
@@ -32,9 +33,26 @@ func CreateBlock(data string, prevHash []byte) *Block {
 
 /*
 We need a Genesis block due to the fact that each block references to a previous block.
+@param 'coinbase' is the first transaction
 */
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{}) //Genesis block will have an empty slice of bytes as the previous Hash
+func Genesis(coinbase *Transaction) *Block {
+	return CreateBlock([]*Transaction{coinbase}, []byte{}) //Genesis block will have an empty slice of bytes as the previous Hash
+}
+
+/*
+Provides a hash for all of the transactions in a block combined.
+*/
+func (b *Block) HashTransactions() []byte {
+	var txHashes [][]byte //2D slice of bytes
+	var txHash [32]byte   //slice of bytes with a length of 32
+
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.ID) //each row of txHashes will be the transaction ID of the current tx in all of the block's transactions
+	}
+
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{})) //putting together each row a a single array of bytes. []bytes{} means that each row will not be separated by anything
+
+	return txHash[:]
 }
 
 /* -------------- SERIALIZATION & DESERIALIZATION -------------- */
